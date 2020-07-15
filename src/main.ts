1901e3ed5183fd8ee3dd7e4e27475203fc80e267
Kristher Vidal <kristher@dripcoder.com>
2020-07-15 10:45:12 +0800

    Change Port

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,24 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from "apollo-server";
 
-import resolvers from './resolvers';
-import typeDefs from './type-defs';
-import { ProductsApi } from './data-source/products-api';
+import resolvers from "./resolvers";
+import typeDefs from "./type-defs";
+import { ProductsApi } from "./data-source/products-api";
 
 const server = new ApolloServer({
-    resolvers,
-    typeDefs,
-    dataSources: () => {
-        return {
-            productsApi: new ProductsApi(),
-        };
-    },
+  resolvers,
+  typeDefs,
+  dataSources: () => {
+    return {
+      productsApi: new ProductsApi(),
+    };
+  },
 });
 
-server.listen()
-    .then(({ url }) => console.log(`Server ready at ${url}. `));
+server
+  .listen({ port: 3212 })
+  .then(({ url }) => console.log(`Server ready at ${url}. `));
 
 if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => server.stop());
+  module.hot.accept();
+  module.hot.dispose(() => server.stop());
 }
