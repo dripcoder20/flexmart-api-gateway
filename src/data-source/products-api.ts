e056dad0051c61128d2d5b702fba57741a12a858
Kristher Vidal <kristher@dripcoder.com>
2020-09-01 22:28:57 +0800

    182 - Get Product Details Query

--- src/data-source/products-api.ts
+++ src/data-source/products-api.ts
@@ -1,19 +1,23 @@
-import { RESTDataSource } from "apollo-datasource-rest"
+import { RESTDataSource } from "apollo-datasource-rest";
+import { ApolloError } from "apollo-server";
 
 export class ProductsApi extends RESTDataSource {
-
   constructor() {
-      super();
-      this.baseURL = process.env.PRODUCTS_URL;
-    }
-  
-  async products({start = 0, limit = 10}) {
-    const option = `_start=${start}&_limit=${limit}`
+    super();
+    this.baseURL = process.env.PRODUCTS_URL;
+  }
+
+  async products({ start = 0, limit = 10 }) {
+    const option = `_start=${start}&_limit=${limit}`;
     return this.get(`products?${option}`);
   }
 
-  async topProducts({start = 0, limit = 10}) {
+  async topProducts({ start = 0, limit = 10 }) {
     const option = `_sort=totalSales:DESC&_start=${start}&_limit=${limit}`;
     return this.get(`products?${option}`);
+  }
+
+  async product({ id }) {
+    return this.get(`products/${id}`);
   }
 }
